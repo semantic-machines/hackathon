f9f378f577bdfbddd21f10f641ee403e6805fddf
Itiua <ValeriyBushenev@gmail.com>
2019-06-29 21:12:49 +0300

    upd install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#echo INSTALL VEDA
-#wget https://github.com/semantic-machines/veda/releases/download/v5.4.6/Veda-x86_64.AppImage
-#chmod u+x Veda-x86_64.AppImage
-#./Veda-x86_64.AppImage
-#./install-tools/install-dbs.sh
-#./install-tools/install-haproxy.sh
-#./control-start.sh
+echo INSTALL VEDA
+wget https://github.com/semantic-machines/veda/releases/download/v5.4.6/Veda-x86_64.AppImage
+chmod u+x Veda-x86_64.AppImage
+./Veda-x86_64.AppImage
+./install-tools/install-dbs.sh
+./install-tools/install-haproxy.sh
+./control-start.sh
 echo INSTALL HACKATON PROJECT
 wget https://github.com/semantic-machines/hackathon/archive/master.zip
 unzip master.zip
