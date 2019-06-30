08205b226476485877a5bf74519beaa3034ffd46
Itiua <ValeriyBushenev@gmail.com>
2019-06-30 15:00:35 +0300

    upd install script

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ echo INSTALL DBs
 ./install-tools/install-dbs.sh
 echo INSTALL HAPROXY
 ./install-tools/install-haproxy.sh
-sudo cp -i ./install-tools/haproxy.cfg /etc/haproxy
+sudo cp -v ./install-tools/haproxy.cfg /etc/haproxy
 echo INSTALL HACKATON PROJECT
-cp public/index.html 
+cp -v public/index.html 
 ./control-start.sh
 pause 30
 wget https://github.com/semantic-machines/hackathon/archive/master.zip
 unzip master.zip
 rm master.zip
-cp hackathon-master/public/index.html public
-cp hackathon-master/ontology/config.ttl ontology
-cp -r hackathon-master/ontology/LNG ontology
+cp -v hackathon-master/public/index.html public
+cp -v hackathon-master/ontology/config.ttl ontology
+cp -v -r hackathon-master/ontology/LNG ontology
