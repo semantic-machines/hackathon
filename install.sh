f9e130cab0b7b32e47fa8b5892ba15d35ec28f53
Itiua <ValeriyBushenev@gmail.com>
2019-06-30 14:41:51 +0300

    upd install script

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
 echo INSTALL VEDA
 wget https://github.com/semantic-machines/veda/releases/download/v5.4.6/Veda-x86_64.AppImage
 chmod u+x Veda-x86_64.AppImage
 ./Veda-x86_64.AppImage
-cd install-tools
-./install-dbs.sh
-./install-haproxy.sh
-sudo cp -i haproxy.cfg /etc/haproxy
-cd ..
+echo INSTALL DBs
+./install-tools/install-dbs.sh
+echo INSTALL HAPROXY
+./install-tools/install-haproxy.sh
+sudo cp -i ./install-tools/haproxy.cfg /etc/haproxy
+echo INSTALL HACKATON PROJECT
 cp public/index.html 
 ./control-start.sh
-echo INSTALL HACKATON PROJECT
+pause 30
 wget https://github.com/semantic-machines/hackathon/archive/master.zip
 unzip master.zip
 rm master.zip
